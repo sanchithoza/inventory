92714065579a32e710f4dc9b54fbd6060053b0fe
Sanchit Oza <sanchithoza@gmail.com>
2021-01-13 15:51:29 +0530

    implimenting reports on client side

--- server/routes/report.js
+++ server/routes/report.js
@@ -1,27 +1,22 @@
-const { PDFDocument, StandardFonts, rgb } = require('pdf-lib')
-let path = require("path");
-
+const { jsPDF } = require("jspdf"); // will automatically load the node version
 
 
 async function routes(fastify, options) {
     fastify.get('/getpdf', async(request, reply) => {
-        const pdfDoc = await PDFDocument.create()
-        const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman)
+        try {
+            const doc = new jsPDF();
+            await doc.html("hello", {
+                callback: function(doc) {
+                    doc.save("a45.pdf");
+                },
+                x: 10,
+                y: 10
+            });
+        } catch (error) {
+            reply.send(error)
+        }
 
-        const page = pdfDoc.addPage()
-        const { width, height } = page.getSize()
-        const fontSize = 30
-        page.drawText('Creating PDFs in JavaScript is awesome!', {
-            x: 50,
-            y: height - 4 * fontSize,
-            size: fontSize,
-            font: timesRomanFont,
-            color: rgb(0, 0.53, 0.71),
-        })
-        const pdfBytes = await pdfDoc.save()
-        reply.type('pdfBytes')
     })
 }
-
 
 module.exports = routes
